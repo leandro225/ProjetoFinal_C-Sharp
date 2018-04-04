c904fa4826df59b3ecbd785cdd6bab1137d0ad39
Leandro <Leandro@DESKTOP-6E04D66>
2018-04-04 20:13:48 -0300

    Adicionando classes

--- ProjetoFinal/Controllers/PedidoController.cs
+++ ProjetoFinal/Controllers/PedidoController.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassLibrary1
+namespace Controllers
 {
-    public class Class1
+    class PedidoController
     {
     }
 }
